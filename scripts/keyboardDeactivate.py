bc999067ac2fe3a3ac0fc444179aa5ad599c3e76
connosieurofdoom <shreyasajitrajendran@gmail.com>
2020-11-05 11:35:40 +0530

    KeyboardDeactivate.py

---
+++ scripts/keyboardDeactivate.py
@@ -0,0 +1,16 @@
+import re
+
+import subprocess
+
+out = subprocess.check_output(['xinput', 'list'], shell= True).strip()
+for line in out.splitlines():
+    
+    print(line.decode())
+
+t = input("Enter id:")
+
+out = subprocess.check_output(['xinput', 'float', t], shell= True).strip()
+
+for line in out.splitlines():
+    
+    print(line.decode())
