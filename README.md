8ff21f7c7aa967185c6ab960aa50c7246a35ed26
shreyas <shreyas@vindiata.com>
2022-01-07 00:56:44 +0530

    1

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # Scripts
 Simple scripts for simple things
+
+### Scripts:
+
+#### Cost Calc:
+File Name: Scripts\\cost_calc.py
+Description: Simple script to calculate costs for different countries.
+
+Function Parameter: val(Can be list or int.).
+Additional inputs: Asks for currency type and number of semesters.
+
+Example 1 :
+```
+conv(1)
+1. for EUR
+2. for Cad
+3. for USD
+Input:1
+Number of Semester:1
+84.32
+
+Example 2:
+```
+conv([144, 790*6])
+1. for EUR
+2. for Cad
+3. for USD
+Input:1
+Number of Semester:4
+1647275.52
+```
--- scripts/cost_calc.py
+++ scripts/cost_calc.py
@@ -1,3 +1,29 @@
+"""
+Simple script to calculate costs for different countries.
+
+Parameter: val.
+Can be list or int.
+Asks for currency type and number of semesters.
+
+Example 1 :
+conv(1)
+1. for EUR
+2. for Cad
+3. for USD
+Input:1
+Number of Semester:1
+84.32
+
+Example 2:
+conv([144, 790*6])
+1. for EUR
+2. for Cad
+3. for USD
+Input:1
+Number of Semester:4
+1647275.52
+"""
+
 eur = 84.32
 cad = 58.35
 usd = 74.47
@@ -6,7 +32,7 @@ def conv(val):
     c = int(input("1. for EUR\n2. for Cad\n3. for USD\nInput:"))
     ct = [eur,cad,usd]
     ctu = ct[c-1]
-    month = int(input("Number of Months:"))
+    month = int(input("Number of Semester:"))
     try:
         if isinstance(val,list):
             return round(sum(ctu*i for i in val)*month,2)
